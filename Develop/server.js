9e5563e2bd413e816a31ca92cc1bab8b270416cd
Shini <halimshini@hotmail.com>
2023-03-15 20:23:51 -0400

    added middleware and routes

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,27 @@ const express = require ('express')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//MIDDLEWARE
+app.use(express.json());
+app.use (express.urlencoded({extended:true}))
 
+//ROUTES
+
+app.get('/notes',(req,res)=>{
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('api/notes',(req,res)=>{
+res.json(db.json)
+}
+)
+
+app.post('api/notes',(req,res)=>{
+})
 
 app.listen(PORT, () => {
     console.log("app listening at http://localhost:${PORT}`")
