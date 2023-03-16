073986eb68a040b65607ce3498bd69850239cdbb
Shini <halimshini@hotmail.com>
2023-03-15 20:18:34 -0400

    Created server.js

---
+++ Develop/server.js
@@ -0,0 +1,12 @@
+//DEPENDENCIES
+const fs =  require ('fs');
+const express = require ('express')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+app.listen(PORT, () => {
+    console.log("app listening at http://localhost:${PORT}`")
+})
