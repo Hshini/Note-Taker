200d2dbd5253ccbc6c00e5c31b9c67ba6d0d6369
Shini <halimshini@hotmail.com>
2023-03-15 21:25:30 -0400

    add fs write file

--- Develop/server.js
+++ Develop/server.js
@@ -4,27 +4,49 @@ const express = require ('express')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const path = require('path');
 //MIDDLEWARE
 app.use(express.json());
 app.use (express.urlencoded({extended:true}))
-
+app.use(express.static('public'))
 //ROUTES
 
-app.get('/notes',(req,res)=>{
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
-})
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
+app.get('/notes',(req,res)=>
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+)
 
-app.get('api/notes',(req,res)=>{
-res.json(db.json)
+
+app.get('/api/notes',(req,res)=>{
+    fs.readFile( './db/db.json', function(error,dataResults){
+        if(error){
+           throw error
+        }
+        else{
+            res.send(dataResults)
+        }
+        
+    } )
+       
 }
 )
 
-app.post('api/notes',(req,res)=>{
+app.post('/api/notes',(req,res)=>{
+//convert 
+    const data=req.body
+    
+    fs.writeFile( './db/db.json', data, function(error) {
+        if(error){
+            throw error
+        }
+        data.
+        res.json("File Succesfuly Created")
+    })
+
+})
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.listen(PORT, () => {
